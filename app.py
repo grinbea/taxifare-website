608854999ddc7c1890a6b38451928171476d0aa6
Alejandra <65710403+grinbea@users.noreply.github.com>
2025-04-04 16:05:38 -0300

    Try with mapbox

--- app.py
+++ app.py
@@ -5,10 +5,37 @@ import datetime
 st.title("Taxifare Model")
 
 ## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-st.text_input("Pickup location")
-st.text_input("Dropoff location")
-st.date_input("Time", datetime.date(2019, 7, 6))
-st.number_input('Persons')
+from = st.text_input("Pickup location")
+to   = st.text_input("Dropoff location")
+date = st.date_input("Time", datetime.date(2019, 7, 6))
+nper = st.number_input('Persons')
+
+# Your Mapbox Access Token
+access_token = 'your_mapbox_access_token'
+
+# Define the search text and endpoint
+search_text = "New York, USA"
+url = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{search_text}.json"
+
+# Parameters for the request
+params = {
+    'access_token': access_token,
+    'limit': 5,  # Limit the number of results returned
+    'country': 'us'  # Limit search to the USA
+}
+
+# Make the GET request
+response = requests.get(url, params=params)
+
+# Check if the request was successful
+if response.status_code == 200:
+    data = response.json()
+    for feature in data['features']:
+        st.markdown(f"Place Name: {feature['place_name']}")
+        st.markdown(f"Coordinates: {feature['geometry']['coordinates']}")
+    else:
+        st.markdown("Error:", response.status_code)
+
 
 if st.button('Submit'):
 #    params = { "pickup_datetime":date,
